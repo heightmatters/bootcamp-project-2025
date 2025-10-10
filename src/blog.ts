c40de86fdf9b51ec30a5dde5a07d8eeaa24e9e3a
Noah <noahschn317@gmail.com>
2025-10-10 13:11:21 -0700

    feat: bug blog.ts

--- src/blog.ts
+++ src/blog.ts
@@ -1,30 +1,27 @@
 type Blog = {
-  title:string;
-  date:string;
-  description:string;
-  image:string(ex."./images/me.png);
-  imageAlt:string;
-  slug:string;
+  title: string;
+  date: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  slug: string;
 };
 
-const blogs:Blog [] = [
+const blogs: Blog[] = [
   {
-    title:"Blog 1",
-    date:"xx/xx/xx",
-    description:"Here is a blog about me",
-    image:"",
-    imageAlt:"",
-    slug:"",
-  };
-
-const blogs:Blog [] = [
+    title: "Blog 1",
+    date: "xx/xx/xx",
+    description: "Here is a blog about me",
+    image: "",
+    imageAlt: "",
+    slug: "",
+  },
   {
-    title:"Blog 2",
-    date:"xx/xx/xx",
-    description:"",
-    image:"",
-    imageAlt:"",
-    slug:"",
-  };
-  
-    
+    title: "Blog 2",
+    date: "xx/xx/xx",
+    description: "Here is a blog about me",
+    image: "",
+    imageAlt: "",
+    slug: "",
+  },
+];
