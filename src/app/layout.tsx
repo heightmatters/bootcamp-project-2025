a843be4321c2e37b621ba18afc03b9ca31c3b907
Noah <noahschn317@gmail.com>
2025-12-12 00:30:43 -0800

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,32 @@
-export const metadata = {
-  title: "Test Layout",
+import type { Metadata } from "next"; // Object to set metadata
+import { Roboto } from "next/font/google"; // You can change the font to anything you want.
+import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+// If you are experiencing an error "localFont is undefined", you might need to add the following blocks of code
+const roboto = Roboto({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Noah Chang's Personal Website",
+  description: "A personal website for Noah Chang.",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
+  // returns boilerplate
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
+
 
