a0220c64d1423980d20cc9106c994d2f7ea1aef3
Noah <noahschn317@gmail.com>
2025-12-11 20:55:05 -0800

    feat: fix api routing

--- bootcamp-milestone-4/src/app/api/blogpages/[slug]/route.ts
+++ bootcamp-milestone-4/src/app/api/blogpages/[slug]/route.ts
@@ -1,22 +1,21 @@
-import { NextRequest, NextResponse } from 'next/server'
-import connectDB from "@/database/db"
-import blogPagesSchema from "@/database/blogPageSchema"
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/database/db";
+import blogPagesSchema from "@/database/blogPageSchema";
 
-type IParams = {
-  params: {
-    slug: string
-  }
-}
+export async function GET(
+  req: NextRequest,
+  context: { params: { slug: string } }
+) {
+  console.log("API RECEIVED SLUG:", context.params.slug);
 
-export async function GET(req: NextRequest, { params }: IParams) {
-  console.log("API RECEIVED SLUG:", params.slug)
-  await connectDB()
-  const { slug } = params
+  await connectDB();
+
+  const slug = context.params.slug;
 
   try {
-    const page = await blogPagesSchema.findOne({ slug }).orFail()
-    return NextResponse.json(page)
+    const page = await blogPagesSchema.findOne({ slug }).orFail();
+    return NextResponse.json(page);
   } catch (err) {
-    return NextResponse.json('Blog page not found.', { status: 404 })
+    return NextResponse.json("Blog page not found.", { status: 404 });
   }
 }
