34a205d744c9dfb0b21aa4423a4329cbaba09d89
Noah <noahschn317@gmail.com>
2025-12-12 00:25:29 -0800

    feat: test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,11 @@
-import type { Metadata } from "next"; // Object to set metadata
-import { Roboto } from "next/font/google"; // You can change the font to anything you want.
-import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
-
-// If you are experiencing an error "localFont is undefined", you might need to add the following blocks of code
-const roboto = Roboto({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Noah Chang's Personal Website",
-  description: "A personal website for Noah Chang.",
+export const metadata = {
+  title: "Test Layout",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  // returns boilerplate
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/components/projectCarousel.tsx
+++ src/components/projectCarousel.tsx
@@ -3,7 +3,7 @@
 
 import React, { useRef, useEffect, useState } from "react";
 import style from "./projectCarousel.module.css";
-import ProjectPreview from "@/components/projectPreview";
+import ProjectPreview from "./projectPreview";
 import Link from "next/link";
 
 export default function Carousel({ projects }: { projects: any[] }) {
